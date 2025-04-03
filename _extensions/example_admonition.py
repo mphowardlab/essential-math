72c54c7b978cc31ea034514db8338212ee5e0def
Michael Howard <mphoward@auburn.edu>
2025-04-03 12:50:08 -0500

    Disable mdformat, add example admonition (#34)

---
+++ _extensions/example_admonition.py
@@ -0,0 +1,12 @@
+from docutils.parsers.rst.directives.admonitions import Admonition
+
+
+class Example(Admonition):
+    """Example admonition."""
+    def run(self):
+        self.arguments[0] = f"Example: {self.arguments[0]}"
+        return super().run()
+
+
+def setup(app):
+    app.add_directive("example", Example)
